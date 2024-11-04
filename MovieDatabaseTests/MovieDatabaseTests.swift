3f50a85e2acacb71fae0cc2aa18bd3c839cbab6a
nathanrubeda <ubedanathan@gmail.com>
2024-11-04 09:25:07 -0500

    Initial Commit

---
+++ MovieDatabaseTests/MovieDatabaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDatabaseTests.swift
+//  MovieDatabaseTests
+//
+//  Created by Nathan Ubeda on 11/4/24.
+//
+
+import Testing
+
+struct MovieDatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
