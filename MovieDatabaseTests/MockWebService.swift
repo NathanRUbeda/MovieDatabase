07cd38921853e84fb9097b4d8a54782b5d21aa6d
NathanRUbeda <ubedanathan@gmail.com>
2024-12-04 11:29:39 -0500

    Add tests for web service.

--- MovieDatabaseTests/MockWebService.swift
+++ MovieDatabaseTests/MockWebService.swift
@@ -6,3 +6,95 @@
 //
 
 import Foundation
+@testable import MovieDatabase
+
+/// An object that mimics an interaction with a cloud service using a JSON file.
+class MockWebService: WebService, MovieProvider {
+	/// Sends request to get movies based on searched input.
+	/// - Parameters:
+	/// - query: A searched word inputted by the user.
+	/// - Returns: A `MovieResponse` object.
+	/// - Throws: A `NetworkError` if unable to build request or encountered during processing of request.
+	func fetchSearchedMovies(query: String) async throws -> MovieDatabase.MovieResponse {
+		let url: URL? = switch query {
+			case "spiderman":
+				Bundle.main.url(forResource: "mockSearchedSpidermanMovieJSON", withExtension: "json")
+			case "whiplash":
+				Bundle.main.url(forResource: "mockSearchedWhiplashMovieJSON", withExtension: "json")
+			case "how to train your dragon":
+				Bundle.main.url(forResource: "mockSearchedTrainYourDragonMovieJSON", withExtension: "json")
+			default:
+				nil
+		}
+		
+		guard let jsonURL = url else {
+			throw NetworkError.missingJSON
+		}
+		
+		guard let data = try? Data(contentsOf: jsonURL) else {
+			throw NetworkError.loadingError
+		}
+		
+		return try decode(data)
+	}
+
+	/// Sends request to get popular movies.
+	/// - Returns: A `MovieResponse` object.
+	/// - Throws: A `NetworkError` if unable to build request or encountered during processing of request.
+	func fetchPopularMovies() async throws -> MovieDatabase.MovieResponse {
+		guard let jsonURL = Bundle.main.url(forResource: "mockPopularMoviesJSON", withExtension: "json") else {
+			throw NetworkError.missingJSON
+		}
+		
+		guard let data = try? Data(contentsOf: jsonURL) else {
+			throw NetworkError.loadingError
+		}
+		
+		return try decode(data)
+	}
+
+	/// Sends request to get now playing movies.
+	/// - Returns: A `MovieResponse` object.
+	/// - Throws: A `NetworkError` if unable to build request or encountered during processing of request.
+	func fetchNowPlayingMovies() async throws -> MovieDatabase.MovieResponse {
+		guard let jsonURL = Bundle.main.url(forResource: "mockNowPlayingMoviesJSON", withExtension: "json") else {
+			throw NetworkError.missingJSON
+		}
+		
+		guard let data = try? Data(contentsOf: jsonURL) else {
+			throw NetworkError.loadingError
+		}
+		
+		return try decode(data)
+	}
+
+	/// Sends request to get top rated movies.
+	/// - Returns: A `MovieResponse` object.
+	/// - Throws: A `NetworkError` if unable to build request or encountered during processing of request.
+	func fetchTopRatedMovies() async throws -> MovieDatabase.MovieResponse {
+		guard let jsonURL = Bundle.main.url(forResource: "mockTopRatedMoviesJSON", withExtension: "json") else {
+			throw NetworkError.missingJSON
+		}
+		
+		guard let data = try? Data(contentsOf: jsonURL) else {
+			throw NetworkError.loadingError
+		}
+		
+		return try decode(data)
+	}
+
+	/// Sends request to get upcoming movies.
+	/// - Returns: A `MovieResponse` object.
+	/// - Throws: A `NetworkError` if unable to build request or encountered during processing of request.
+	func fetchUpcomingMovies() async throws -> MovieDatabase.MovieResponse {
+		guard let jsonURL = Bundle.main.url(forResource: "mockUpcomingMoviesJSON", withExtension: "json") else {
+			throw NetworkError.missingJSON
+		}
+		
+		guard let data = try? Data(contentsOf: jsonURL) else {
+			throw NetworkError.loadingError
+		}
+		
+		return try decode(data)
+	}
+}
--- MovieDatabaseTests/MovieWebService_Tests.swift
+++ MovieDatabaseTests/MovieWebService_Tests.swift
@@ -6,30 +6,91 @@
 //
 
 import XCTest
+@testable import MovieDatabase
 
 final class MovieWebService_Tests: XCTestCase {
+	var webService: MovieProvider?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+		webService = MockWebService()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+		webService = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+	func test_WebService_fetchSearchedMovies_shouldFetchSearchedMovies() async throws {
+		// Given
+		guard let webService else {
+			XCTFail()
+			return
+		}
+		
+		// When
+		let queries = ["spiderman", "whiplash", "how to train your dragon"]
+		
+		// Then
+		for query in queries {
+			let response = try await webService.fetchSearchedMovies(query: query)
+			XCTAssertNotNil(response)
+		}
+	}
+	
+	func test_WebService_fetchPopularMovies_shouldFetchPopularMovies() async throws {
+		// Given
+		guard let webService else {
+			XCTFail()
+			return
+		}
+		
+		// When
+		let response = try await webService.fetchPopularMovies()
+		
+		// Then
+		XCTAssertNotNil(response)
+	}
+	
+	func test_WebService_fetchNowPlayingMovies_shouldFetchNowPlayingMovies() async throws {
+		// Given
+		guard let webService else {
+			XCTFail()
+			return
+		}
+		
+		// When
+		let response = try await webService.fetchNowPlayingMovies()
+		
+		// Then
+		XCTAssertNotNil(response)
+	}
+	
+	func test_WebService_fetchTopRatedMovies_shouldFetchTopRatedMovies() async throws {
+		// Given
+		guard let webService else {
+			XCTFail()
+			return
+		}
+		
+		// When
+		let response = try await webService.fetchTopRatedMovies()
+		
+		// Then
+		XCTAssertNotNil(response)
+	}
+	
+	func test_WebService_fetchUpcomingMovies_shouldFetchUpcomingMovies() async throws {
+		// Given
+		guard let webService else {
+			XCTFail()
+			return
+		}
+		
+		// When
+		let response = try await webService.fetchUpcomingMovies()
+		
+		// Then
+		XCTAssertNotNil(response)
+	}
 }
